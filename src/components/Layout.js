1afe8f487e40876dab64205dc904a5c78e767b12
Ran Marciano <ranm13@gmail.com>
2019-10-31 17:47:30 +0200

    made logo component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
+import Logo from './main/Logo';
 
 class Layout extends Component {
     render() {
         return (
         <div>
-    
+            <Logo />
         </div>)
     }
 }
---
+++ src/components/main/Logo.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import logo from './logo.png'
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+class Logo extends Component {
+    render() {
+        return (
+        <Grid container justify="center" alignItems="center">
+            <img src={logo} alt="Bank Hashualim logo" width="150px"></img>
+            <Typography variant="h1">
+                Bank Hashualim
+            </Typography>
+        </Grid>)
+    }
+}
+export default Logo
